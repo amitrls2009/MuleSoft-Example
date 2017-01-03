8fe6aec2e3430184de3297c6f9bc59aa351f3b31
Amit Kumar <amitrls2009@gmail.com>
2017-01-03 22:01:51 +0530

    Added File for Order

---
+++ Project/src/test/resources/Order.java
@@ -0,0 +1,79 @@
+package meridukanorder;
+
+import java.lang.Comparable;
+
+public class Order implements Comparable<Order>{
+
+	int orderNumber;
+	String orderDate;
+	String requiredDate;
+	String shippedDate;
+	String status;
+	String comments;
+	int customerNumber;
+	public int getOrderNumber() {
+		return orderNumber;
+	}
+
+	public void setOrderNumber(int orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+
+	public String getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(String orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public String getRequiredDate() {
+		return requiredDate;
+	}
+
+	public void setRequiredDate(String requiredDate) {
+		this.requiredDate = requiredDate;
+	}
+
+	public String getShippedDate() {
+		return shippedDate;
+	}
+
+	public void setShippedDate(String shippedDate) {
+		this.shippedDate = shippedDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	public int getCustomerNumber() {
+		return customerNumber;
+	}
+
+	public void setCustomerNumber(int customerNumber) {
+		this.customerNumber = customerNumber;
+	}
+
+	@Override
+	public int compareTo(Order o) {
+		int comapareorderNumber = ((Order) o).getOrderNumber();
+
+		//ascending order
+		return this.orderNumber - comapareorderNumber;
+	}
+}
+
+
